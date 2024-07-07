4d0243073c468671edfb5eb316855aa16366fd3f
xj <8172473+x35a@users.noreply.github.com>
2024-07-07 21:13:45 +0300

    up

--- DOM.js
+++ DOM.js
@@ -1,9 +1,14 @@
 /*
 DOM Inheritance
 
+From js perspective every entity is an Object Type.
+
 Object: {
     EventTarget: {
         Node: {
+            // Node (and Element) is not only for html but for xml, svg, etc. 
+            // Each Node has own type: https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType 
+
             Element: {
                 HTMLElement: {
                     HTML{Tag}Element: {
@@ -15,7 +20,6 @@ Object: {
     }
 }
 
-From js perspective every entity is an Object type.
 
 https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
 https://uk.javascript.info/basic-dom-node-properties
