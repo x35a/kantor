c191357309320eaf52fbdea3e6a6a4b473e4fd11
xj <8172473+x35a@users.noreply.github.com>
2024-07-08 01:47:17 +0300

    up

--- DOM.js
+++ DOM.js
@@ -11,17 +11,22 @@ Object: {
             // Node is abstract i can't call any Node directly eg: document.parentNode is null but Node provides some props and methods to navigate and change dom tree.
             // Each Node has type, node types: https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType 
 
-            Document: {
-// document is a type of Node 
-// API to access each line of the document like tags, comments, bare text, whitespaces, line breaks, etc.
-                https://developer.mozilla.org/en-US/docs/Web/API/Document
+            DocumentType: {
+                // DocumentType is a type of Node
+                // represents doctype https://developer.mozilla.org/en-US/docs/Web/API/DocumentType
+            }
 
-https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Using_the_Document_Object_Model#what_is_a_dom_tree
+            Document: {
+                // document is a type of Node 
+                // document is API to access each line of the document like tags, comments, bare text, whitespaces, line breaks, etc.
+
+                https://developer.mozilla.org/en-US/docs/Web/API/Document
+                https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Using_the_Document_Object_Model#what_is_a_dom_tree
                 https://developer.mozilla.org/en-US/docs/Web/API/HTML_DOM_API
             }
 
             Element: {
-// is a type of Node 
+                // is a type of Node 
                 // Element provides props and methods for html, xml, svg, etc.
                  
                 HTMLElement: {
@@ -31,15 +36,16 @@ https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Using_the
                 }
             }
 
-document type
-Attribute 
-Text
-Comment
-
+            Attribute: {}
+            Text: {}
+            Comment: {}
         }
 
         Window: {
             https://developer.mozilla.org/en-US/docs/Web/API/Window
+
+            window.HTMLDocument is alias on for Document for historical reasons https://developer.mozilla.org/en-US/docs/Web/API/HTMLDocument
+
         }
     }
 }
