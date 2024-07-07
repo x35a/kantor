ea29f860680a27fdbc6788a09311463eabc3bec0
xj <8172473+x35a@users.noreply.github.com>
2024-07-07 21:45:00 +0300

    up

--- DOM.js
+++ DOM.js
@@ -6,10 +6,12 @@ From js perspective every entity is an Object Type.
 Object: {
     EventTarget: {
         Node: {
-            // Node (and Element) is not only for html but for xml, svg, etc. 
+            // Node is abstract i can't call any Node directly eg: document.parentNode is null but Node provides some props and methods to navigate and change dom tree.
             // Each Node has own type: https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType 
 
             Element: {
+                // Element is not only for html but for xml, svg, etc.
+                 
                 HTMLElement: {
                     HTML{Tag}Element: {
                         // eg: HTMLInputElement
@@ -21,6 +23,6 @@ Object: {
 }
 
 
-https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
+inheritance chain https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
 https://uk.javascript.info/basic-dom-node-properties
 */
