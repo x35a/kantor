6385bfc7733fc03ac3033d0a1d8338f443b66997
xj <8172473+x35a@users.noreply.github.com>
2024-07-08 12:07:39 +0300

    up

--- DOM.js
+++ DOM.js
@@ -2,9 +2,9 @@
 DOM Inheritance
 
 > Each part of xml/html/svg file content (tags, attrs, comments, bare text, whitespaces, line breaks, etc) converts into Node object then Nodes composing into tree structure.
+> For js each Node is a js object.
 > Each Node has some Node Type.
 > Dom is a general idea about how to represent documents (eg xml/html/svg) thats why Node is an abstract that is not direct equivalent to html tag for example.
-> For js each Node is a js object.
 
 
 Object: {
