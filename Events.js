a4d115962fe7cb0a799e9ec14b8c5c97b5bfabd5
xj <8172473+x35a@users.noreply.github.com>
2024-07-09 22:20:53 +0300

    Update Events.js

--- Events.js
+++ Events.js
@@ -3,7 +3,7 @@ How to add event handlers:
 
 1 in tag attribute 
 <p onclick="dosmth()">
-brauser wraps onclick code in self run function so we need to run dosmth explicitly.
+brawser wraps onclick code in self run function so we need to run dosmth explicitly.
 
 2 in element dom object prop
 element.onclick = function () {...}
@@ -11,6 +11,9 @@ element.onclick = function () {...}
 elem.onclick = null // remove
 
 3 addEventListener
+removeEventListener
+++ support many handlers on same event on same element.
+thera some events that can be handled using addEventListener only.
 
 https://uk.javascript.info/introduction-browser-events
 */
