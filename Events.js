406f5c29f015bf1858a64b10015c23c202454adc
xj <8172473+x35a@users.noreply.github.com>
2024-07-10 00:39:37 +0300

    up

--- Events.js
+++ Events.js
@@ -16,13 +16,21 @@ removeEventListener
 thera some events that can be handled using addEventListener only.
 
 
+There are many event types like click, input, keydown etc.
+There are some events groups in mdn like: 
+Device Orientation Events, Pointer Events, Server Sent Events, Touch Events, UI Events
+Search 'events' word here https://developer.mozilla.org/en-US/docs/Web/API to found event groups.
+
 EventTarget
 is a root dom interface that provides methods: addEventListener(), removeEventListener(), dispatchEvent()
 
+
+
 Event
-is an object that is passing into handler function when event happens. // eventHandler (event) {...}
+is an event object that is automatically passed as argument into handler function when event happens. it has some specific info about this event. // eventHandler (event) {...}
 https://uk.javascript.info/introduction-browser-events#obyekt-podiyi
 https://developer.mozilla.org/en-US/docs/Web/API/Event
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#event_objects
 
 
 Events have similar inheritance chain for example click:
