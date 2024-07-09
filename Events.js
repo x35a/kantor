e7d0cbbb7cbcb9363b5f673a65de78ff2db2052a
xj <8172473+x35a@users.noreply.github.com>
2024-07-09 21:49:03 +0300

    Update Events.js

--- Events.js
+++ Events.js
@@ -1,7 +1,11 @@
+/*
 How to add events:
 
 1 tag attribute 
+<p onclick="dosmth()">
+brauser wraps onclick code in self run function so we need to run dosmth explicitly.
 
 2 dom object 
 
 3 addEventListener
+*/
