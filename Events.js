7c5ff743911453ac55176e5d6f90e1a8235c60f7
xj <8172473+x35a@users.noreply.github.com>
2024-07-10 01:34:43 +0300

    up

--- Events.js
+++ Events.js
@@ -21,17 +21,18 @@ There are some events groups in mdn like:
 Device Orientation Events, Pointer Events, Server Sent Events, Touch Events, UI Events
 Search 'events' word here https://developer.mozilla.org/en-US/docs/Web/API to found event groups.
 
-Each tag has some events. The list of events it has depends on its dom interface and what interfaces it inherits, see dom interface inheritance.
+Each tag has some events like click etc. The list of events it has depend on its dom interface and what interfaces it inherits, see dom interface inheritance.
+Methods like addEventListener(), removeEventListener(), dispatchEvent() tag inherits from EventTarget which is a root dom interface. https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
 
-
-EventTarget
-is a root dom interface that provides methods: addEventListener(), removeEventListener(), dispatchEvent()
-
-Event
-is an event object that is automatically passed as argument into handler function when event happens. it has some specific info about this event. // eventHandler (event) {...}
+When event happens and handler runs, the handler function gets Event object as its argument. // eventHandler (event) {...}
+Event object passed automatically and it has props about this event like:
+event.type
+event.target // reference to the object to which the event was originally dispatched. (who has fired event)
 https://uk.javascript.info/introduction-browser-events#obyekt-podiyi
 https://developer.mozilla.org/en-US/docs/Web/API/Event
 https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#event_objects
+
+What is inside Event object?
 
 
 Events have similar inheritance chain for example click:
