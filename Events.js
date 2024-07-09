cf7d8a0abcacc3ea82f4a62df4c5af455ffed3fd
xj <8172473+x35a@users.noreply.github.com>
2024-07-09 21:58:00 +0300

    Update Events.js

--- Events.js
+++ Events.js
@@ -1,14 +1,14 @@
 /*
-How to add events:
+How to add event handlers:
 
-1 tag attribute 
+1 in tag attribute 
 <p onclick="dosmth()">
 brauser wraps onclick code in self run function so we need to run dosmth explicitly.
 
-2 element dom object prop
+2 in element dom object prop
 element.onclick = function () {...}
 -- can't add many onclick on single element cause there's only one onclick prop in element.
-
+elem.onclick = null // remove
 
 3 addEventListener
 
