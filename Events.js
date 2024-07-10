bba5a5117c65ae90c6ee0f0022d8a3e7db0fc937
xj <8172473+x35a@users.noreply.github.com>
2024-07-10 11:40:12 +0300

    up

--- Events.js
+++ Events.js
@@ -27,9 +27,7 @@ Each tag has some events like click etc. The list of events it has depend on its
 Methods like addEventListener(), removeEventListener(), dispatchEvent() come from EventTarget which is a root dom interface. https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
 
 When event happens and eventhandler function runs, the handler function gets Event object as its argument. // eventHandler (event) {...}
-Event object passed automatically and it has props about this event like:
-event.type
-event.target // reference to the object to which the event was originally dispatched. (who has fired event)
+Event object passed automatically and it has props about this event like event.type etc.
 https://uk.javascript.info/introduction-browser-events#obyekt-podiyi
 https://developer.mozilla.org/en-US/docs/Web/API/Event
 https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#event_objects
@@ -42,4 +40,18 @@ Event < UIEvent < MouseEvent < PointerEvent
 https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
 List of Event interfaces is here https://developer.mozilla.org/en-US/docs/Web/API/Event#interfaces_based_on_event
 Each Event interface provide some props and methods and depends on what type of event happend the event inherits some Event interfaces with theirs props and methods.
+
+
+Bubbling
+https://uk.javascript.info/bubbling-and-capturing
+
+form > div > p
+if click event happens on p elements then it happens on div then on form and up to document - it is bubbling.
+it doen't matter if elements have event listeners or hadlers, click event fires and bubbling every time the click happens.
+to catch the click event we add eventListener and handlers to do smth on this event.
+if div and form have on click listener and some handlers, they fire and handlers run if click happed on p element no matter does p have on click listener on not.
+
+
+event.target // reference to the object to which the event was originally dispatched. (who has fired event)
+
 */
