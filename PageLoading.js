1ec3ce8ab86290f1e61fc6ebc6029a6e1efc00d2
xj <8172473+x35a@users.noreply.github.com>
2024-07-12 16:26:53 +0300

    Update PageLoading.js

--- PageLoading.js
+++ PageLoading.js
@@ -1,3 +1,10 @@
 /*
+document.readyState values:
+loading // dom is not ready 
+interactive // dom is ready but scripts, styles, images are not 
+complete // everything is loaded 
 
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState
+https://uk.javascript.info/onload-ondomcontentloaded
 */
