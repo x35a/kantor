41296c3523bb3a62157980c765fd90b8a4e12280
xj <8172473+x35a@users.noreply.github.com>
2024-07-14 13:06:36 +0300

    Update PageLoading.js

--- PageLoading.js
+++ PageLoading.js
@@ -1,12 +1,19 @@
 /*
-document.readyState values:
+document.readyState
+values:
 loading // dom is not ready 
 interactive // dom is ready but scripts, styles, images are not 
 complete // everything is loaded 
 
 document.readyState can check document state at any time.
 
-
 https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState
-https://uk.javascript.info/onload-ondomcontentloaded
+
+readystatechange event
+document.addEventListener('readystatechange', (event) => {console.log(document.readyState)})
+fires each time when document changes it state.
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/readystatechange_event
+
+https://uk.javascript.info/onload-ondomcontentloaded#readystate
 */
