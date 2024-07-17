007407229e07e7e0485ea3b3987853ccc2464095
xj <8172473+x35a@users.noreply.github.com>
2024-07-18 01:35:02 +0300

    Update Canvas.js

--- Canvas.js
+++ Canvas.js
@@ -8,12 +8,27 @@ Drawing context is a way how browser draws something.
 There are several drawing contexts:
 2d, webgl1, webgl2, webgpu
 
-getContex() method allows to select the context // canvasElement.getContext('2d')
+Each context has its own api with bunch of props, methods, events dedicated for drawing.
 
+getContex() method allows to select the context // canvasElement.getContext('2d')
 
 HTMLCanvasElement
 https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
 
 getContext()
 https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext#contexttype
+
+2d
+2d context is a CanvasRenderingContext2D interface which provides api for drawing 2d shapes.
+Relies on cpu mostly during rendering.
+Usecase: simple 2d graphics and animations.
+
+CanvasRenderingContext2D
+https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D#reference
+Canvas tutorial
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+
+webgl
+
+
 */
