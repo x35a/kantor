1308a0c295beb7803004a74cd3f2bb5d089e0a50
xj <8172473+x35a@users.noreply.github.com>
2024-07-18 01:10:21 +0300

    Update Canvas.js

--- Canvas.js
+++ Canvas.js
@@ -1,3 +1,19 @@
 /*
+Canvas tag is a HTMLCanvasElement dom interface with some unique methods and events.
 
+Before drawing i must select one of the drawing context.
+
+Drawing context is a way how browser draws something.
+
+There are several drawing contexts:
+2d, webgl1, webgl2, webgpu
+
+getContex() method allows to select the context // canvasElement.getContext('2d')
+
+
+HTMLCanvasElement
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
+
+getContext()
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext#contexttype
 */
